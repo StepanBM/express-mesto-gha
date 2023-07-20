8addb7506709985c3ac09af0209f97dbefa663a2
Stepan Belenov <Sabaneev99@yandex.ru>
2023-07-20 21:58:33 +0300

    Test4 №14

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
+const { errors } = require('celebrate');
+
 const userRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
 
@@ -13,8 +15,17 @@ const { login, addUser } = require('./controllers/users');
 
 const { validatorLogin, validatorAddUser } = require('./middlewares/validator');
 
+const serverError = require('./errors/ServerError');
+
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.post('/signin', validatorLogin, login);
+app.post('/signup', validatorAddUser, addUser);
+
+app.use(serverError);
+app.use(errors());
+app.use(auth);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -22,11 +33,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/signin', validatorLogin, login);
-app.post('/signup', validatorAddUser, addUser);
-
-app.use(auth);
 
 app.use('/users', userRoutes);
 app.use('/cards', cardsRoutes);
